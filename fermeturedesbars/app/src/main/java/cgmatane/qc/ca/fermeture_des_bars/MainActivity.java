cb27e895c672318dccfa85f8495932ce4d1c1fbc
gui45 <guillaume.d.marceau@gmail.com>
2018-11-20 08:49:08 -0500

    ajouter projet

---
+++ fermeturedesbars/app/src/main/java/cgmatane/qc/ca/fermeture_des_bars/MainActivity.java
@@ -0,0 +1,18 @@
+package cgmatane.qc.ca.fermeture_des_bars;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import org.apache.cordova.*;
+import org.apache.cordova.Config;
+import org.apache.cordova.DroidGap;
+
+public class MainActivity extends DroidGap {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.init();
+        super.loadUrl(Config.getStartUrl());
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
