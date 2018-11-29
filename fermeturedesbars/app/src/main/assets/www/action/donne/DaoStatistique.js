f801b1233b4b40e8a3949e0ad7f038e57432a933
gui45 <guillaume.d.marceau@gmail.com>
2018-11-29 17:23:49 -0500

    vue statistique a tester

---
+++ fermeturedesbars/app/src/main/assets/www/action/donne/DaoStatistique.js
@@ -0,0 +1,14 @@
+var DaoStatistique = function(){
+  this.recupererToutLesStatistique = function(){
+    return localStorage['statistiques'];
+  }
+  this.ajouterPartie(partie){
+    if(localStorage['statistiques']){
+      donnees = localStorage['statistiques'];
+      donnees['statistique'][donnees.length] = partie;
+      localStorage['statistiques'] = donnees;
+    }else{
+      localStorage['statistiques'][0] = partie;
+    }
+  }
+}
--- fermeturedesbars/app/src/main/assets/www/action/vue/VueStatistique.js
+++ fermeturedesbars/app/src/main/assets/www/action/vue/VueStatistique.js
@@ -1,10 +1,18 @@
 var VueStatistique = function(){
   var contenuPage;
-  constructeur = function(){
+  innitialiser = function(){
     contenuPage = document.getElementById("statistique").innerHTML;
   }
   this.afficher = function(){
     document.body.innerHTML = contenuPage;
+    conteneur = document.getElementById("statistique");
+    daoStatistique = new DaoStatistique();
+    donnees = daoStatistique.recupererToutLesStatistique();
+    html = "";
+    for(int iStatistique = 0; iStatistique < donnees.lenght; i++){
+      html += "<span><p>"+donnes[i].nom+"</p><p>"+donnees[i].score+"</p></span>";
+    }
+    conteneur.innerHTML = html;
   }
-  constructeur();
+  innitialiser();
 }
