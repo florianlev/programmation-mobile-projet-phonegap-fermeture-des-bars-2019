26b24016110e074fcfe40405c01c55320d7106a2
Bassirou <haidara.bassirou@yahoo.com>
2018-12-02 21:44:36 -0500

    recuperation du formulaire apreciation

--- fermeturedesbars/app/src/main/assets/www/action/vue/VueApreciation.js
+++ fermeturedesbars/app/src/main/assets/www/action/vue/VueApreciation.js
@@ -8,8 +8,34 @@ var VueApreciation = function () {
 
   this.afficher = function () {
     document.body.innerHTML = contenuPage;
+    var formulaireAjouter = document.getElementById("formulaire-ajouter");
+    formulaireAjouter.addEventListener("submit",enregistrerEquipe);
   }
+  var enregistrerEquipe = function(evenement)
+          {
 
-  initialiser();
+              evenement.preventDefault();
+
+              var nom = document.getElementById("nom").value;
+              var courriel = document.getElementById("courriel").value;
+              var telephone = document.getElementById("phone").value;
+              var nas = document.getElementById("nas").value;
+              var commentaire = document.getElementById("commentaire").value;
+
+              if(document.getElementById("oui").checked)
+                    {
+                        var aimer = document.getElementById("oui").value;
+                        alert(aimer);
+                    }
+
+		          else if(document.getElementById("non").checked)
+                    {
+                        var aimer = document.getElementById("non").value;
+                        alert(aimer);
+                    }
+
+              initialiser();
+          }
+
 
 }
