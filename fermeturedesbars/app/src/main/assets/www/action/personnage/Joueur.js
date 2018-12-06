2b8b0565f40e3602a10ad5f2b3606ffaa28e1c46
gui45 <guillaume.d.marceau@gmail.com>
2018-12-06 17:14:50 -0500

    ameliorer navigation joueur

--- fermeturedesbars/app/src/main/assets/www/action/personnage/Joueur.js
+++ fermeturedesbars/app/src/main/assets/www/action/personnage/Joueur.js
@@ -2,21 +2,36 @@ function Joueur(scene){
     var joueur = this;
     var positionCourante = {x:0, y:0};
     var cercle;
-
+    var distanceDoit;
+    var fantome;
     function initialiser(){
         // dessiner cercle
+        distanceDoit = 80;
         cercle = new createjs.Shape();
         cercle.graphics.beginFill("red").drawCircle(0,0,50);
         cercle.x = 100;
         cercle.y = 100;
+
+        fantome = new createjs.Shape();
+        fantome.graphics.beginFill("black").drawCircle(0,0,50);
+        fantome.graphics.beginFill("white").drawCircle(0,0,25);
+        fantome.alpha = 0.5;
+        fantome.x = 100;
+        fantome.y = window.innerHeight/2 + 100;
+
         scene.addChild(cercle);
+        scene.addChild(fantome);
       }
     this.setPosition = function(x,y){
       //  cercle.set({x:position.x,y:position.y});
       differenceY = window.innerHeight/2;
-      if((cercle.x - x) < 30 && (cercle.x - x) > -30){
-        cercle.x = x
-        cercle.y =  y - differenceY;
+      y = y - differenceY;
+      if((cercle.x - x) < distanceDoit && (cercle.x - x) > -distanceDoit && (cercle.y - y) < distanceDoit && (cercle.y - y) > -distanceDoit && y > 0){
+        cercle.x = x;
+        cercle.y = y;
+
+        fantome.x = x;
+        fantome.y = y+differenceY;
       }
     }
 
