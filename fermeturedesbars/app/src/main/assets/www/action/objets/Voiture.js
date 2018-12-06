e202bc56f43135d3cdfee988f5fcf6ee66482ccb
Sedrick <sedrickidd@hotmail.com>
2018-12-06 17:39:25 -0500

    Ajout de la voiture affichable

--- fermeturedesbars/app/src/main/assets/www/action/objets/Voiture.js
+++ fermeturedesbars/app/src/main/assets/www/action/objets/Voiture.js
@@ -1,13 +1,26 @@
-var Voiture = function(){
+var Voiture = function(scene){
+  var voiture = this;
+  var imgVoiture = new Image();
+  var bitmapVoiture
+  
   function initialiser(){
-
+    imgVoiture.src = "images/voiture.png";
+    imgVoiture.onload = terminerChargement;
   }
+  function terminerChargement()
+  {
+    bitmapVoiture = new createjs.Bitmap(imgVoiture);
+    bitmapVoiture.scaleX = 0.2;
+    bitmapVoiture.scaleY = 0.2;
+    voiture.afficher();
+  }
+
 
   this.afficher = function () {
-
+    scene.addChild(bitmapVoiture);
+    bitmapVoiture.x = 200;
+    bitmapVoiture.y = 300;
   }
-  this.raffraichirMatrice = function(){
-
-  }
+ 
   initialiser();
 }
