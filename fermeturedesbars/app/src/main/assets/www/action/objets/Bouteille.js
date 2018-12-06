26361ab12575e9eb8c0e555fd3bab9d41ea80a8b
Sedrick <sedrickidd@hotmail.com>
2018-12-06 17:10:11 -0500

    Ajout de la bouteille affichable

--- fermeturedesbars/app/src/main/assets/www/action/objets/Bouteille.js
+++ fermeturedesbars/app/src/main/assets/www/action/objets/Bouteille.js
@@ -1,13 +1,24 @@
-var Bouteille = function(){
+var Bouteille = function(scene){
+  var bouteille = this;
+  var imgBouteille = new Image();
+  var bitmapBouteille;
   function initialiser(){
-
-  }
-
-  this.afficher = function () {
-
-  }
-  this.raffraichirMatrice = function(){
     
+    imgBouteille.src = "images/beer.png";
+    imgBouteille.onload = terminerChargement;
   }
+  function terminerChargement()
+  {
+    bitmapBouteille = new createjs.Bitmap(imgBouteille);
+    bitmapBouteille.scaleX = 0.05;
+    bitmapBouteille.scaleY = 0.05;
+    bouteille.afficher();
+  }
+  this.afficher = function () {
+    scene.addChild(bitmapBouteille);
+    bitmapBouteille.x = 200;
+    bitmapBouteille.y = 100;
+  }
+  
   initialiser();
 }
