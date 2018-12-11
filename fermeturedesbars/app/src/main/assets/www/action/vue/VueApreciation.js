c563af586e1dbb7920db181ef68fa91cc929ddad
Florian <flolevy33@gmail.com>
2018-12-10 22:52:42 -0500

    comment de vueAppreciation

--- fermeturedesbars/app/src/main/assets/www/action/vue/VueApreciation.js
+++ fermeturedesbars/app/src/main/assets/www/action/vue/VueApreciation.js
@@ -1,45 +1,39 @@
-var AjouterVueApreciation = (function(){
+var AjouterVueApreciation = function () {
     var pageAjouterApreciation = document.getElementById("apreciation").innerHTML;
 
-    return function(actionEnregistrerApreciation)
-    {
 
-        this.afficher = function()
-        {
-            document.getElementsByTagName("body")[0].innerHTML = pageAjouterApreciation;
+    this.afficher = function () {
+        document.getElementsByTagName("body")[0].innerHTML = pageAjouterApreciation;
 
-            var formulaireAjouter = document.getElementById("formulaireapreciation");
-            formulaireAjouter.addEventListener("submit",enregistrerApreciation);
+        var formulaireAjouter = document.getElementById("formulaireapreciation");
+        formulaireAjouter.addEventListener("submit", enregistrerApreciation);
 
+    }
+
+    var enregistrerApreciation = function (evenement) {
+
+        evenement.preventDefault();
+
+        var nom = document.getElementById("nom").value;
+        var courriel = document.getElementById("courriel").value;
+        var telephone = document.getElementById("phone").value;
+        var nas = document.getElementById("nas").value;
+        var commentaire = document.getElementById("commentaire").value;
+
+        if (document.getElementById("oui").checked) {
+            var aimer = document.getElementById("oui").value;
+            //  alert(aimer);
         }
 
-        var enregistrerApreciation = function(evenement)
-        {
-
-            evenement.preventDefault();
-
-              var nom = document.getElementById("nom").value;
-              var courriel = document.getElementById("courriel").value;
-              var telephone = document.getElementById("phone").value;
-              var nas = document.getElementById("nas").value;
-              var commentaire = document.getElementById("commentaire").value;
-
-              if(document.getElementById("oui").checked)
-                    {
-                        var aimer = document.getElementById("oui").value;
-                      //  alert(aimer);
-                    }
-
-		          else if(document.getElementById("non").checked)
-                    {
-                        var aimer = document.getElementById("non").value;
-                      //  alert(aimer);
-                    }
-
-            
-
-            actionEnregistrerApreciation();
+        else if (document.getElementById("non").checked) {
+            var aimer = document.getElementById("non").value;
+            //  alert(aimer);
         }
 
-    };
-})();
+
+
+        actionEnregistrerApreciation();
+    }
+
+}
+
