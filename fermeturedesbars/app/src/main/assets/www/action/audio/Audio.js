6a8e5a72fad00f54ca34f2f7eece5ca20f5ef4cb
Sedrick <sedrickidd@hotmail.com>
2018-12-20 17:11:09 -0500

    Deplacement des sons dans une classe en cours

---
+++ fermeturedesbars/app/src/main/assets/www/action/audio/Audio.js
@@ -0,0 +1,16 @@
+var Audio = function()
+{
+    this.chanson = chanson;
+    this.rammaserBiere = rammaserBiere;
+    this.voiture = voiture;
+    this.vomi = vomi;
+    this.heurterVoiture = heurterVoiture;
+    this.heurterCone = heurterCone;
+    initialiser()
+    {
+        
+    }
+
+
+    initialiser();
+}
