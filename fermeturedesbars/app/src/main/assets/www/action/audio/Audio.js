623a6f5abf2f86f7b6c007bd0ded6b47b3bfccb9
Sedrick <sedrickidd@hotmail.com>
2018-12-20 23:54:42 -0500

    Reglage du fichier audio

--- fermeturedesbars/app/src/main/assets/www/action/audio/Audio.js
+++ fermeturedesbars/app/src/main/assets/www/action/audio/Audio.js
@@ -1,47 +1,29 @@
 var Audio = function()
 {
-  this.chanson;
-  this.rammaserBiere;
-  this.voiture;
-  this.vomi;
-  this.heurterVoiture;
-  this.heurterCone;
-    function initialiser()
-    {
-            this.chanson = new Howl({
-            src: ['action/audio/sons/bensound-badass.mp3'],
-            volume : 0.3,
-            loop: true
-          });
-          this.rammaserBiere = new Howl({
-              src:['action/audio/sons/BoireBierre.mp3'],
-              volume: 0.3,
-              loop: false
-          });
-          this.vomi = new Howl({
-              src: ['action/audio/sons/Vomi.mp3'],
-              volume: 0.3,
-              loop: false
-          });
-          this.heurterVoiture = new Holw({
-              src: ['action/audio/sons/HeurterVoiture.mp3'],
-              volume: 0.3,
-              loop: false
-          });
-          this.voiture = new Holw({
-            src: ['action/audio/sons/Voiture.mp3'],
-            volume: 0.3,
-            loop: false
-        });
-        this.heurterCone = new Holw({
-            src: ['action/audio/sons/HeurterCone.mp3'],
-            volume: 0.3,
-            loop: false
-        });
+  this.chanson = new Howl({
+    src: ['action/audio/sons/bensound-badass.mp3'],
+    volume : 0.3,
+    loop: true,  
+    });
+  this.rammaserBiere = new Howl({
+    src:['action/audio/sons/BoireBierre.mp3'],
+    volume: 0.8
+    });
+  this.voiture = new Howl({
+    src: ['action/audio/sons/Voiture.mp3'],
+    volume: 0.3
+    });
+  this.vomi = new Howl({
+    src: ['action/audio/sons/Vomi.mp3'],
+    volume: 0.3   
+    });
+  this.heurterVoiture = new Howl({
+    src: ['action/audio/sons/HeurterVoiture.mp3'],
+    volume: 0.3
+    });;
+  this.heurterCone =  new Howl({
+    src: ['action/audio/sons/HeurterCone.mp3'],
+    volume: 0.3
+    });
 
-
-    }
-
-
-   // initialiser();
 }
